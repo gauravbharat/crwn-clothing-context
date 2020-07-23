902c6202df09c699ae669fd7175bff0e2cfbd964
Gaurav Mendse <gauravmendse@Gauravs-MacBook-Pro.local>
2020-07-23 17:07:43 +0530

    Initial commit

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,50 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: 'AIzaSyBFdlGs6GZpsx-1MdboQu3t8q8dXzrHz6k',
+  authDomain: 'garyd-crwn-db.firebaseapp.com',
+  databaseURL: 'https://garyd-crwn-db.firebaseio.com',
+  projectId: 'garyd-crwn-db',
+  storageBucket: 'garyd-crwn-db.appspot.com',
+  messagingSenderId: '305653774286',
+  appId: '1:305653774286:web:02ed14c48f358a13355d9f',
+  measurementId: 'G-FPQX54RF02',
+};
+
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
